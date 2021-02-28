86dd91cae9ec09914c3cde709aaf733e7a3300af
achraf.jhidri <achrafjh@gmail.com>
2021-02-28 21:03:01 +0100

    avancement

--- src/Controller/GammeController.php
+++ src/Controller/GammeController.php
@@ -5,8 +5,12 @@ namespace App\Controller;
 
 
 use App\Entity\Gamme;
+
+use App\Entity\Produit;
+use App\Form\GammeType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -43,13 +47,46 @@ class GammeController extends AbstractController
     }
 
     /**
+     * @return Response
+     * @Route("/create", name="create_gamme")
+     */
+    public function createAction(Request $request)
+    {
+        $gamme = new Gamme();
+        $form = $this->createForm(GammeType::class,$gamme);
+
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid() ) {
+
+            $gamme = $form->getData();
+
+            $this->em->persist($gamme);
+            $this->em->flush();
+            return $this->render('gamme/show.html.twig', [
+                'gamme'=> $gamme
+            ]);
+        }
+
+        return $this->render('gamme/new.html.twig', [
+        'form'=> $form->createView(),
+            'numberOfGammes' => sizeof($this->em->getRepository(Gamme::class)->findAll())
+        ]);
+    }
+
+    /**
      * @param int $id
-     * @Route("/{id}", name="show_on_gamme")
+     * @Route("/{id}", name="show_one_gamme")
      *
      */
     public function showAction(int $id)
     {
-
+        $produits = $this->em->getRepository(Produit::class)->getProduitsByGamme($id);
+       return $this->render('gamme/show.html.twig',[
+            'gamme' => $this->em->getRepository(Gamme::class)->find($id),
+            'produits' => $produits
+        ]);
     }
 
 }
---
+++ web/js/fos_js_routes.js
@@ -0,0 +1,1 @@
+fos.Router.setData({"base_url":"","routes":{"produit_create":{"tokens":[["text","\/produits\/create"]],"defaults":[],"requirements":[],"hosttokens":[],"methods":["POST","GET"],"schemes":[]}},"prefix":"","host":"localhost","port":"","scheme":"http","locale":[]});
